2a697a79226de609f77ee04cb505bcfdd6da0fbf
Tania Kolesnik <kolesnik@gmail.com>
2021-04-15 21:48:44 +0100

    Initial Commit

---
+++ Merklerex/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Merklerex
+//
+//  Created by Tania Kolesnik on 15/04/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
