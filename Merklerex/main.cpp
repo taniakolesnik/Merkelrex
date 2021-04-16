a49f8601ed856baf4e6c97878ad2f2b398aa40df
Tania Kolesnik <kolesnik@gmail.com>
2021-04-16 21:58:07 +0100

    split into basic functions

--- Merklerex/main.cpp
+++ Merklerex/main.cpp
@@ -7,11 +7,7 @@
 
 #include <iostream>
 
-int main(){
-    
-    while (true) {
-
-    // 1. print help
+static void printMenu() {
     std::cout << "1: Print help" << std::endl;
     // 2. print exchange stats
     std::cout << "2: Print exchange stats" << std::endl;
@@ -24,39 +20,80 @@ int main(){
     // 6. continue
     std::cout << "6: Continue" << std::endl;
     
+}
+
+static int getUserOption() {
     // ask user to choose oprion:
-    std::cout << "====================" << std::endl;
+    // std::cout << "====================" << std::endl;
     std::cout << "Type in 1-6:" << std::endl;
     
-    // read user reply
     int userOption;
     std::cin >> userOption;
+    std::cout << "====================" << std::endl;
     std::cout << "You chose: " << userOption << std::endl;
-    
-    // check reply
+    return userOption;
+}
+
+static void printHelp() {
+    std::cout << "Help - choose options from the menu" << std::endl;
+    std::cout << "and follow the on screen instructions." << std::endl;
+}
+
+static void printMarketStats() {
+    std::cout << "Market looks good" << std::endl;
+}
+
+static void enterAsk() {
+     std::cout << "Make an offer for ..." << std::endl;
+}
+
+static void enterBid() {
+    std::cout << "Make an bid for XXXX enter the amount:" << std::endl;
+}
+
+static void printWallet() {
+    std::cout << "Your wallet: " << std::endl;
+}
+
+static void gotoNextTimeframe() {
+    std::cout << "Continue" << std::endl;
+}
+
+static void processUserOption(int userOption) {
     switch (userOption){
         case 1:
-            std::cout << "Help - choose options from the menu" << std::endl;
-            std::cout << "and follow the on screen instructions." << std::endl;
+            printHelp();
             break;
         case 2:
-            std::cout << "Market looks good" << std::endl;
+            printMarketStats();
             break;
         case 3:
-            std::cout << "Make an offer for ..." << std::endl;
+            enterAsk();
             break;
         case 4:
-            std::cout << "Make an bid for XXXX enter the amount:" << std::endl;
+            enterBid();
             break;
         case 5:
-            std::cout << "Your wallet: " << std::endl;
+            printWallet();
             break;
         case 6:
-            std::cout << "Continue" << std::endl;
+            gotoNextTimeframe();
             break;
-        default: std::cout << "Invalid choice. Choose betwen 1 to 6 and press Enter: " << std::endl; // bad intput
+        default: std::cout << "Invalid choice. Choose 1-6:" << std::endl; // bad intput
     }
+    std::cout << "====================" << std::endl;
+}
+
+int main(){
+    
+    while (true) {
+
+    // 1. print help
+        printMenu();
+    // read user reply
+        int userOption = getUserOption();
+    // check reply
+        processUserOption(userOption);
     }
     return 0;
-    
 }
